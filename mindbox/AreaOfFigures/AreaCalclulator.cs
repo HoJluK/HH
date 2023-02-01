97401021c2c9a0cfd0c059a53f69aadb8ba29437
Vladislav Alekseev <55919069+HoJluK@users.noreply.github.com>
2023-02-01 11:47:05 +0300

    Add files via upload

---
+++ mindbox/AreaOfFigures/AreaCalclulator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AreaOfFigures.Figures;
+
+namespace AreaOfFigures
+{
+    public static class AreaCalculator
+    {
+        // Определяет фигуру по количеству сторон и возвращает её площадь
+        public static double FigureAreaCalculating(Side[] sides)
+        {
+            switch (sides.Length)
+            {
+                case 0:
+                    throw new FigureDefinitionException("Can't define figure with 0 sides.");
+                case 1: //круг
+                    Circle circle;
+                    circle = new Circle(sides);
+
+                    return circle.CalculateArea();
+
+                case 3: //треугольник
+                    Triangle triangle;
+                    triangle = new Triangle(sides);
+
+                    return triangle.CalculateArea();
+
+                default:
+                    throw new FigureDefinitionException("Can't define figure with " + sides.Length + " sides.");
+            }
+        }
+
+
+    }
+    
+    // Если метод не может определить фигуру, выбрасывает FigureDefinitionException
+    public class FigureDefinitionException : Exception
+    {
+        public FigureDefinitionException(string message)
+            : base(message) { }
+    }
+
+}
+
